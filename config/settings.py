9924180d499cd6c31bd678bf6899ded22d0f7a3f
DeveloperBhin <norbeth559@gmail.com>
2025-12-11 12:51:52 +0300

    modify

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-ar0etm%5_@c1s8%=0bsn7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='https://apizuuhportfolio.deploy.tz', cast=Csv())
 
 
 # Application definition
